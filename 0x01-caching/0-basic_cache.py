2379ebabd70920310ea03ed5b203dff88bc99ec8
Harrison <harrisonchialuka@gmail.com>
2024-10-30 17:09:46 +0100

    task 0 to task 2 correction

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,3 +12,21 @@ class BasicCache(BaseCaching):
     """
     Class that defines the caching system
     """
+    def put(self, key, item):
+        """ 
+        Add an item in the cache
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+#        raise NotImplementedError("put must be implemented in your cache class")
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
+#        raise NotImplementedError("get must be implemented in your cache class")
--- 0x01-caching/base_caching.py
+++ 0x01-caching/base_caching.py
@@ -26,17 +26,10 @@ class BaseCaching():
         """ 
         Add an item in the cache
         """
-        if key is None or item is None:
-            pass
-        else:
-            self.cache_data[key] = item
-#        raise NotImplementedError("put must be implemented in your cache class")
+        raise NotImplementedError("put must be implemented in your cache class")
 
     def get(self, key):
         """
         Get an item by key
         """
-        if key is None:
-            return None
-        return self.cache_data.get(key)
-#        raise NotImplementedError("get must be implemented in your cache class")
+        raise NotImplementedError("get must be implemented in your cache class")
